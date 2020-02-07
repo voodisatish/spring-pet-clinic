0a0e9633f08f1bd12dd967cc6007a737bc5491fd
Voodi Sateesh <voodi_sateesh@optum.com>
2020-02-07 06:30:00 +0530

    Added index page for vet and owner index and controller closes #7 #13

---
+++ pet-Clinic-Web/src/main/java/com/vss/springpetclinic/controllers/OwnersController.java
@@ -0,0 +1,16 @@
+package com.vss.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnersController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
+
+
