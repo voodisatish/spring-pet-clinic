dea38ad4fb48b8b7b1db66dff5854a16c65b1120
Voodi Sateesh <voodi_sateesh@optum.com>
2020-02-07 07:19:49 +0530

    Added owner list to index page. closes #16

--- pet-Clinic-Web/src/main/java/com/vss/springpetclinic/controllers/OwnersController.java
+++ pet-Clinic-Web/src/main/java/com/vss/springpetclinic/controllers/OwnersController.java
@@ -1,6 +1,8 @@
 package com.vss.springpetclinic.controllers;
 
+import com.vss.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
